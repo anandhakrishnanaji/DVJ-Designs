76d8ee3fb915259b17b58070d5bf83f5daf0c2ec
anandhakrishnanaji <creattech2000@gmail.com>
2020-09-06 11:43:09 +0530

    Added RegistrationPage Working

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import './pages/homePage.dart';
+import 'pages/loginPage.dart';
+import './pages/registrationPage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          primaryColor: Colors.black,
+          accentColor: Colors.grey[600],
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          inputDecorationTheme: InputDecorationTheme(
+              contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+              border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black, width: 5)),
+              focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black, width: 3)))),
+      home: RegistrationPage(),
+    );
+  }
+}
---
+++ lib/pages/registrationPage.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+
+class RegistrationPage extends StatefulWidget {
+  @override
+  _RegistrationPageState createState() => _RegistrationPageState();
+}
+
+class _RegistrationPageState extends State<RegistrationPage> {
+  Map<String, bool> _check = {
+    'Architect/Interior Designer': false,
+    'Trader': false,
+    'Individual': false
+  };
+  bool _nameerr = false,
+      _phoneerr = false,
+      _emailerr = false,
+      _companyerr = false;
+
+  TextEditingController _name, _phone, _email, _company;
+
+  @override
+  void initState() {
+    super.initState();
+    _name = new TextEditingController();
+    _phone = new TextEditingController();
+    _email = new TextEditingController();
+    _company = new TextEditingController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomPadding: false,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Image.asset('assets/images/logo.png'),
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20),
+              child: Text('Sign In', style: TextStyle(fontSize: 30)),
+            ),
+            TextField(
+              controller: _name,
+              decoration: InputDecoration(
+                  labelText: 'Enter your Name',
+                  errorText: _nameerr ? 'Required Field' : null),
+            ),
+            TextField(
+              controller: _phone,
+              decoration: InputDecoration(
+                  errorText: _phoneerr ? 'Required Field' : null,
+                  labelText: 'Enter your Mobile'),
+            ),
+            TextField(
+              controller: _email,
+              decoration: InputDecoration(
+                  errorText: _emailerr ? 'Required Field' : null,
+                  labelText: 'Enter your Email id'),
+            ),
+            TextField(
+              controller: _company,
+              decoration: InputDecoration(
+                  errorText: _companyerr ? 'Required Field' : null,
+                  labelText: 'Enter your Company Name'),
+            ),
+            Container(
+              height: 170,
+              child: Column(
+                children: _check.keys
+                    .map((String key) => CheckboxListTile(
+                        title: Text(key),
+                        value: _check[key],
+                        onChanged: (bool value) {
+                          setState(() {
+                            _check[key] = value;
+                          });
+                        },
+                        controlAffinity: ListTileControlAffinity.leading))
+                    .toList(),
+              ),
+            ),
+            Container(
+              color: Colors.black,
+              child: MaterialButton(
+                  child: Text(
+                    'Submit',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      _nameerr = _name.text.isEmpty;
+                      _phoneerr = _phone.text.isEmpty;
+                      _emailerr = _email.text.isEmpty;
+                      _companyerr = _company.text.isEmpty;
+                    });
+                    //if(!_nameerr&&!_phoneerr&&!_emailerr&&!_companyerr) Navigator.of(context).pushNamed(routeName);
+                  }),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
