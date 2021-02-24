e27450659ec00ff2c37609a5f323e8ea07ee5ea4
anandhakris <creattech2000@gmail.com>
2021-02-24 21:35:11 +0530

    kk

--- lib/widgets/orderDetailsWidget.dart
+++ lib/widgets/orderDetailsWidget.dart
@@ -5,8 +5,6 @@ class OrderDetailsWidget extends StatelessWidget {
   OrderDetailsWidget(this.orderDetails);
   @override
   Widget build(BuildContext context) {
-    final String og_string =
-        orderDetails['enquiry_product'].split('<br>').join('\n\n');
     final height = MediaQuery.of(context).size.height;
     return Container(
       padding: EdgeInsets.only(top: 0.03 * height),
@@ -21,20 +19,6 @@ class OrderDetailsWidget extends StatelessWidget {
             ),
           ),
           Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              'Enquiry Name: ${orderDetails['enquiry_name']}',
-              style: TextStyle(fontSize: 18),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              'Enquiry Mobile: ${orderDetails['enquiry_mobile']}',
-              style: TextStyle(fontSize: 18),
-            ),
-          ),
-          Padding(
             padding: const EdgeInsets.only(top: 12),
             child: Text(
               'Ordered Products:\n',
@@ -45,12 +29,26 @@ class OrderDetailsWidget extends StatelessWidget {
             ),
           ),
           Padding(
-            padding: const EdgeInsets.all(3),
-            child: Text(
-              og_string,
-              style: TextStyle(fontSize: 15),
-            ),
-          )
+              padding: const EdgeInsets.all(5),
+              child: Column(
+                children: orderDetails['order_details']
+                    .map<Widget>((e) => Container(
+                          decoration: BoxDecoration(
+                              border: Border.all(color: Colors.black)),
+                          child: ListTile(
+                            leading: Container(
+                              height: 50,
+                              width: 50,
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      image: AssetImage(
+                                          Uri.decodeFull(e['product_image'])))),
+                            ),
+                            title: Text(e['producName']),
+                          ),
+                        ))
+                    .toList(),
+              ))
         ],
       ),
     );
